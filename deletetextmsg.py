b6fa0194fd19a33d57adb6ef70466a6b24005b48
PREET MODH <preetmodh@gmail.com>
2020-10-22 16:00:40 +0530

    Update deletetextmsg.py

--- deletetextmsg.py
+++ deletetextmsg.py
@@ -9,11 +9,16 @@ Created on Wed May 13 18:31:07 2020
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC74b523a2be1ca45cedb6ad493ac2631d"
+account_sid = "YOUR ACCOUNT SID FROM THE WEBSITE"
 # Your Auth Token from twilio.com/console
-auth_token = '9b2bb7f181c726941213f75b7fba0626'
+account_sid = "YOUR ACCOUNT SID FROM THE WEBSITE"
 
 client = Client(account_sid, auth_token)
 
 for msg in client.messages.list():
     msg.delete()
+
+    
+    
+    
+   
